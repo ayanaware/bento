c892821ecea163c6efd75b040dfb4dbb7b723072
TheAkio <me@theak.io>
2018-10-06 20:30:58 +0200

    Subscribe decorator uses constructor property instead of metadata now

--- lib/decorators/Subscribe.ts
+++ lib/decorators/Subscribe.ts
@@ -1,21 +1,24 @@
 'use strict';
 
-import { Meta } from './Meta';
-
 export function Subscribe(primaryComponent: string, eventName: string): MethodDecorator {
 	return function (target: any, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) {
 		if(target.prototype !== undefined) {
 			throw new Error(`The subscribe decorator can only be applied to non-static class methods ("${propertyKey}" in class "${target.name}")`);
 		}
 
-		const subscriptions = Meta.getSubscriptions(target);
+		if (target.constructor._subscriptions == null) {
+			Object.defineProperty(target.constructor, '_subscriptions', {
+				configurable: false,
+				enumerable: false,
+				writable: false,
+				value: [],
+			});
+		}
 
-		subscriptions.push({
+		target.constructor._subscriptions.push({
 			primaryComponent,
 			eventName,
 			listener: descriptor.value,
 		});
-
-		Meta.setSubscriptions(target, subscriptions);
 	};
 }

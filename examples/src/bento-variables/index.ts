39b9a4196401b1625d3b9ca42e594d0ef3a1e5be
Randon <randon@dvfl.co>
2018-11-14 12:47:08 -0500

    v1.0.0-alpha5

--- examples/src/bento-variables/index.ts
+++ examples/src/bento-variables/index.ts
@@ -11,7 +11,7 @@
 import { Bento, ComponentAPI, ConfigLoader, Variable } from '@ayana/bento';
 
 import { Logger } from '@ayana/logger';
-const log = Logger.get('null');
+const log = Logger.get(null);
 
 // first we start by creating a new bento instance
 const bento = new Bento();
@@ -22,14 +22,14 @@ const bento = new Bento();
 	bento.setVariable('hello', 'world');
 
 	// using config loader plugin to map an ENV variable to a bento variable
-	log.info('Mapping bento variable "foo" to env variable "bar"');
+	log.info('Mapping bento variable "foo" to env variable "BAR"');
 
 	// tell config loader to do the mapping
 	const config = new ConfigLoader();
 	config.addDefinition({
 		name: 'foo', // the name of the bento variable to map to
 		env: 'BAR', // the name of the env variable
-		value: 'Try running this command with FOO=mystring in front', // You dont need this, just setting this to hopefully get users to test `FOO=mystring node ...`
+		value: 'TRY RUNNING THIS EXAMPLE WITH `BAR=mystring` IN FRONT', // You dont need this, just setting this to hopefully get users to test `FOO=mystring node ...`
 	});
 
 	// load config loader into bento
@@ -48,15 +48,28 @@ const bento = new Bento();
 		private hello: string;
 
 		async onLoad() {
-			log2.info(`Component property hello (mapped to bento variable hello) = ${this.hello}`);
+			log2.info(`Decorator defined variable "hello" = "${this.hello}"`);
 
 			// define it in onLoad
+			this.api.defineVariable({
+				type: 'string',
+				name: 'foo',
+				default: 'bar',
+			});
 
-			// or grab it in onLoad
+			// IMPORTANT, by defining a varaible with a default it means even if the variable is not loaded into bento
+			// api.getVariable will return the default
+
+			// or just grab it
 			const foo = await this.api.getVariable('foo');
-			log2.info(`Variable foo = ${foo}`);
+			log2.info(`Variable foo = "${foo}"`);
 		}
 	}
+
+	const instance = new ExampleComponent();
+
+	// load the component
+	await bento.addPrimaryComponent(instance);
 })().catch(e => {
 	log.error('Uh-oh it looks like this example is broken! Please let someone know.');
 	console.log(e);
